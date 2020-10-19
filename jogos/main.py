cfc130ec05842f219b11132bf8f4427fea6230ca
José Ricardo lima dos santos pereira <jricardo@trustprev.com.br>
2020-10-19 10:01:16 -0300

    Jogo da Adivinhação
    O uso de for e ifs

--- jogos/main.py
+++ jogos/main.py
@@ -1,1 +1,29 @@
-print("Bem vindo ao jogo de Adivinhar")
+print("**********************************")
+print("Bem vindo no jogo de Adivinhação")
+print("**********************************")
+
+numero_secreto = 42
+total_de_tentativas = 3
+
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o seu numero: ")
+    print("Você digitou", chute_str)
+    chute = int(chute_str)
+    if (chute < 1 or chute > 100):
+        print("você deve digitar um número entre 1 e 100 !")
+        continue
+
+        acertou = chute == numero_secreto
+        maior = chute > numero_secreto
+        menor = chute < numero_secreto
+
+    if (acertou):
+        print("você acertou")
+        break
+    else:
+        if (maior):
+            print("Você errou")
+
+        elif (menor):
+            print("Você errou")

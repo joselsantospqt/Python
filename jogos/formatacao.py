1a16283cfef4852f3f34df35b2cb842e7176f4ff
José Ricardo lima dos santos pereira <jricardo@trustprev.com.br>
2020-10-19 10:02:11 -0300

    interpolação de strings

---
+++ jogos/formatacao.py
@@ -0,0 +1,25 @@
+print("tentativas {} de {} vezes".format(3,10))
+
+print("R$ {}".format(1.59))
+
+print("R$ {:f}".format(1.59))
+
+print("R$ {:.2f}".format(1.59))
+
+print("R$ {:.2f}".format(1.5))
+
+print("R$ {:.2f}".format(1234.5))
+
+print("R$ {:7.2f}".format(1234.5))
+
+print("R$ {:7.2f}".format(4.5))
+
+print("R$ {:07.2f}".format(4.5))
+
+print("R$ {:07d}".format(46))
+
+print("DATA {:2d}/{:2d}".format(9,4))
+
+print("DATA {:02d}/{:02d}".format(9,4))
+
+print("DATA {:02d}/{:02d}".format(19,11))

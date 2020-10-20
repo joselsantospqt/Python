1a9d646e27108591b32b122df66f2abebb841c8e
José Ricardo lima dos santos pereira <jricardo@trustprev.com.br>
2020-10-20 11:43:10 -0300

    Interpolação de string

--- jogos/formatacao.py
+++ jogos/formatacao.py
@@ -23,3 +23,18 @@ print("DATA {:2d}/{:2d}".format(9,4))
 print("DATA {:02d}/{:02d}".format(9,4))
 
 print("DATA {:02d}/{:02d}".format(19,11))
+
+
+print("Ola Sr.{1} {0}".format("Cordeiro","Leonardo"))
+
+print("R$ {:7.1f}".format(1000.12))
+
+print("R$ {:07.2f}".format(4.11))
+
+print("R$ {:09.2f}".format(1000.19))
+
+print("R$ {:07.1f}".format(1000.16))
+
+nome = "Ricardo"
+print(f'Menu nome é {nome}')
+print(f'Meu nome é {nome.lower()}')

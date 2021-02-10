3ae746e2b44250033fe393c9905406d7a3846aa8
Jose.lsantos <jose.lsantos@al.infnet.edu.br>
2021-02-10 07:13:40 -0300

    TP 1 - Ezequiel
    TP 1 - Ezequiel

---
+++ Desenvolvimento Python para Sistemas Operacionais e Redes/TP1.py
@@ -0,0 +1,67 @@
+def rodar(func):
+    if __name__ == '__main__':
+        print('-' * 10, func.__name__, '-' * 10)
+        func()
+
+    return func
+
+
+def questao1():
+    print('teste')
+
+def questao2():
+    print('teste')
+
+def questao3():
+    print('teste')
+
+def questao4():
+    print('teste')
+
+def questao5():
+    print('teste')
+
+def questao6():
+    print('teste')
+
+def questao7():
+    print('teste')
+
+def questao8():
+    print('teste')
+
+def questao9():
+    print('teste')
+
+def questao10():
+    print('teste')
+
+def questao11():
+    print('teste')
+
+def questao12():
+    print('teste')
+
+def questao13():
+    print('teste')
+
+def questao14():
+    print('teste')
+
+def questao15():
+    print('teste')
+
+def questao16():
+    print('teste')
+
+def questao17():
+    print('teste')
+
+def questao18():
+    print('teste')
+
+def questao19():
+    print('teste')
+
+def questao20():
+    print('teste')

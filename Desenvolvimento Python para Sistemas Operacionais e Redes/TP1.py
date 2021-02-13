236a18e02cf050f167c5bd82b1dc8467e6219dc8
José Ricardo lima dos santos pereira <jricardo@trustprev.com.br>
2021-02-13 03:56:45 -0300

    Exercícios 1-10
    Exercícios 1-10

--- Desenvolvimento Python para Sistemas Operacionais e Redes/TP1.py
+++ Desenvolvimento Python para Sistemas Operacionais e Redes/TP1.py
@@ -1,3 +1,8 @@
+import os
+import datetime
+import psutil
+
+
 def rodar(func):
     if __name__ == '__main__':
         print('-' * 10, func.__name__, '-' * 10)
@@ -5,40 +10,75 @@ def rodar(func):
 
     return func
 
-
 def questao1():
-    print('teste')
+    nome = os.getlogin()
+    print(nome)
 
 def questao2():
-    print('teste')
+    "a - São chaves com valores e cada uma delas é representada em um dicionário"
+
+    key = os.environ
+    print(key)
+    "print(os.environ['USERNAME'])"
+
+    print(os.environ['USERPROFILE'])
 
 def questao3():
-    print('teste')
+
+    pid = os.getpid()
+    print(pid)
 
 def questao4():
-    print('teste')
+    caminho_Absoluto = os.getcwd()
+    print(caminho_Absoluto)
 
 def questao5():
-    print('teste')
+    arquivo = "texto.txt"
+    if os.path.exists(arquivo):
+        print(arquivo, "existe !")
+    else:
+        print(arquivo, "Não existe !")
 
 def questao6():
-    print('teste')
+    arquivo = "texto.txt"
+    caminho_Absoluto = os.getcwd()
+    extension = os.path.splitext(arquivo)[0]
+    print(extension)
+
 
 def questao7():
-    print('teste')
+    caminho_Absoluto = os.getcwd()
+    arquivo = "C:\\Users\\José Ricardo\\Documents\Python\\Desenvolvimento Python para Sistemas Operacionais e Redes\\texto.txt"
+    print(os.path.dirname((arquivo)))
+
 
 def questao8():
-    print('teste')
+    lista = os.listdir()
+    caminho_Absoluto = os.getcwd() + "\\"
+    for i in lista:
+        print("Arquivo: " + i)
+        print(os.stat(caminho_Absoluto + i).st_size)
+
 
 def questao9():
-    print('teste')
+    lista = os.listdir()
+    caminho_Absoluto = os.getcwd() + "\\"
+    for i in lista:
+        print("Arquivo: " + i)
+        print(os.stat(caminho_Absoluto + i).st_size)
+        print("Data de Criação:")
+        print(datetime.datetime.fromtimestamp(os.stat(caminho_Absoluto + i).st_ctime))
+        print("Data de modificação :")
+        print(datetime.datetime.fromtimestamp(os.stat(caminho_Absoluto + i).st_mtime))
 
 def questao10():
-    print('teste')
-
+    "A diferença os.exec apenas inicia um processo sem retornar nada ao processo chamador."
+    "A diferença está nos primeiro e último argumentos de os.spawnv."
+    "O primeiro argumento indica que é para esperar o processo terminar"
+    " antes de retornar ao processo que criou."
+@rodar
 def questao11():
-    print('teste')
-
+ 
 def questao12():
     print('teste')
 

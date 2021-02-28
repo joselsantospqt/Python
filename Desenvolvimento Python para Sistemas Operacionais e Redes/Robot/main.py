8471cfa6c4b2a4be954e0faa61763ae3021ffc2d
José Ricardo <jose.lsantos@al.infnet.edu.br>
2021-02-28 05:59:34 -0300

    Isolando a KEY, Enviando, recebendo e ações do send_chat_action
    Isolando a KEY, Enviando, recebendo e ações do send_chat_action

--- Desenvolvimento Python para Sistemas Operacionais e Redes/Robot/main.py
+++ Desenvolvimento Python para Sistemas Operacionais e Redes/Robot/main.py
@@ -1,12 +1,36 @@
+import time
 import telegram
-from telegram.ext import Updater
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from Robot import config
 
-TOKEN ='1693982914:AAEp5Tf7HwmcY7SAyTzU-yM1pGVktJ6OGnw'
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
 
-updater = Updater(token='TOKEN', use_context=False)
+
+
+updater = Updater(token=config.TOKEN, use_context=True)
+
+def msg(update, context):
+    chat_id = update.effective_chat.id
+    texto = update.effective_messag.text
+    context.bot.send_message(chat_id=chat_id, text='processando')
+    context.bot.send_chatt_action(chat_id=chat_id, action='typing')
+    time.sleep(2)
+    context.bot.send_message(chat_id=chat_id, text=texto.upper())
+
+
+
 
 def start(update, context):
-    chat_id = update.effective_chat.chat.id
+    chat_id = update.effective_chat.id
     context.bot.send_message(chat_id=update.effective_chat.id, text=f"Bem Vindo {chat_id}")
 
+msg_handler = MessageHandler(Filters.text, msg)
+
+start_handler = CommandHandler('start', start)
+updater.dispatcher.add_handler(start_handler)
+updater.dispatcher.add_handler(msg_handler)
+
+updater.start_polling()
 
